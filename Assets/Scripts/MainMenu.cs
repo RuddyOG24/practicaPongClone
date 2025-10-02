497cb002eeab8000bede4297b77ff1f5ad04a8b1
x-PARISH-x <hernanfloosalcedo21@gmail.com>
2025-10-02 12:45:13 -0400

    MusicAudioGame
    b

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public AudioSource backgroundMusic;
+
+    void Start()
+    {
+        if (backgroundMusic != null && !backgroundMusic.isPlaying)
+        {
+            backgroundMusic.loop = true;
+            backgroundMusic.Play();
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("GameScene");  
