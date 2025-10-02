8dc3970ce26c1e09fb208b4e70ee554fb8ca886e
x-PARISH-x <hernanfloosalcedo21@gmail.com>
2025-10-02 12:21:15 -0400

    MainMenuComplete
    a

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");  
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Salir del juego");
+        Application.Quit();
+    }
+}
